836b34292ee2f9c27544045ff76c016ba52a03dc
SriHarshaBarkuru <sriharsha.barkuru@oneconvergence.com>
2023-08-19 18:55:10 +0530

    Update hf_client.py

--- rayserve/HF/client/hf_client.py
+++ rayserve/HF/client/hf_client.py
@@ -1,5 +1,6 @@
 # Usage: python hf_client.py <uuid> <text to be completed>
-# Ex: python hf_client.py 8165b659-e9ba-4ee7-b396-a10effe4818c "dog is "
+# Usage: python hf_client.py <profile> <name> <text to be completed>
+# Ex: python hf_client.py default hf-biogpt "dog is "
 
 
 import configparser
@@ -13,23 +14,24 @@ config = configparser.ConfigParser()
 
 
 # Check if a parameter is provided
-if len(sys.argv) > 2:
+if len(sys.argv) > 3:
     param1 = sys.argv[1]
     param2 = sys.argv[2]
+    param3 = sys.argv[3]
 else:
-    print("Please provide uuid.")
+    print("Please provide name.")
     exit(1)
 
 # get http url & token
-ini_file = "/tmp/.d3x.ini"
+ini_file = "/userdata/.d3x.ini"
 config.read(ini_file)
-url = config.get("default","url")
-token = config.get("default","auth-token")
+url = config.get(param1,"url")
+token = config.get(param1,"auth-token")
 
 
 # get deployment details
 headers = {'Authorization': token}
-r = requests.get(f"{url}/llm/api/deployments/{param1}", headers=headers, verify=False)
+r = requests.get(f"{url}/llm/api/deployments/{param2}", headers=headers, verify=False)
 deployment = r.json()['deployment']
 
 # get serving details
@@ -38,6 +40,6 @@ SERVING_ENDPOINT = f"{url}{deployment['endpoint']}"
 
 # serving request
 headers={'Authorization': SERVING_TOKEN}
-resp = requests.post(SERVING_ENDPOINT, json={"prompt": param2}, headers=headers, verify=False)
+resp = requests.post(SERVING_ENDPOINT, json={"prompt": param3}, headers=headers, verify=False)
 print (resp.text)
 
