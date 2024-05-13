50df34559610cb36cdfe724117e0f2374221f0f2
Deepro Mallick <deepro.mallick@oneconvergence.com>
2024-05-13 14:00:40 +0530

    added fix for "page unknown" error in .json, .htm and .html ingestions

--- rag/ingestion/custom_sdr.py
+++ rag/ingestion/custom_sdr.py
@@ -8,7 +8,7 @@ from document_loaders import document_loader_func
 
 def get_meta(file_path):
   _, extension = os.path.splitext(file_path)
-  if extension == ".txt":
+  if extension in [".txt", ".json", ".htm", ".html"]:
        return {"page_label": "1","file_name": os.path.basename(file_path)}
   return {"file_name": os.path.basename(file_path)}
 
